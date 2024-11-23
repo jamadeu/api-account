dde5453969c4f99cc5cf89476a346c9cfd32e728
Jean Amadeu <jean.amadeu2@gmail.com>
2024-11-23 16:03:41 -0300

    doc: add swagger

--- router/routes.go
+++ router/routes.go
@@ -4,15 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jamadeu/api-account/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
 	v1 := router.Group(basePath)
 	{
 		v1.GET("", func(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"message": "test ok"})
 		})
 	}
+
+	// Initialize Swagger
+	router.GET("/swagger/*any",
+		ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
