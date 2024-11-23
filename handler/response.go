1c635d5d5a77aab77e52d6dea57048011c20f85a
Jean Amadeu <jean.amadeu2@gmail.com>
2024-11-23 16:45:56 -0300

    feat: add functions to responses

---
+++ handler/response.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SendError(ctx *gin.Context, code int, msg string) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message":   msg,
+		"errorCode": code,
+	})
+}
+
+func SendSuccess(ctx *gin.Context, code int, op string, data interface{}) {
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(code, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfull", op),
+		"data":    data,
+	})
+}
